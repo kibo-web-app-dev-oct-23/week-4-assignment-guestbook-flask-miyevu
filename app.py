358f88637cecdca93df2a7f6b9679bc9879287eb
Keklem <keklemyevu@gmail.com>
2023-11-10 21:11:16 +0000

    Yes!

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ def get_db():
     conn = sqlite3.connect(DATABASE)
     return conn
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS guests (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                message TEXT NOT NULL
+            )
+        ''')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # If we send POST request to create user
@@ -20,10 +33,48 @@ def index():
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
+    
+        if not name or not email or not message:
+            flash('All fields must be filled.', 'error')
+        else:
+            # Insert data into the database
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute('''
+                INSERT INTO guests (name, email, message) VALUES (?, ?, ?)
+            ''', (name, email, message))
+            db.commit()
+            flash('Entry submitted successfully!', 'success')
+            return redirect(url_for('index'))
 
     if request.method == 'GET':
-        return
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT name, email, message FROM guests")
+        entries = cursor.fetchall()
+        db.close()
+        return render_template('view_guestbook.html', entries=entries)  
     # If we send GET request to get all users
     # add logic
 
     return render_template('index.html')
+
+@app.route('/view_guestbook')
+def view_guestbook():
+    # Connect to the database
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Retrieve all guestbook entries from the database
+    cursor.execute("SELECT name, email, message FROM guests")
+    entries = cursor.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    return render_template('view_guestbook.html', entries=entries)
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
